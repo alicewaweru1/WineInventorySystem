e2b5e61cbf432cdef2b220c6980db72860f8afb8
alice-waweru <alicewaweru003@5676>
2026-06-26 06:36:54 +0300

    add statistics card

--- WineInventorySystem/frontend/dashboard.py
+++ WineInventorySystem/frontend/dashboard.py
@@ -37,3 +37,65 @@ subtitle = tk.Label(
 )
 
 subtitle.pack()
+
+
+stats_frame = tk.Frame(root, bg=BLACK)
+stats_frame.pack(pady=20)
+
+
+card1 = tk.Frame(
+    stats_frame,
+    bg=CARD,
+    width=250,
+    height=130,
+    highlightbackground=GOLD,
+    highlightthickness=2
+)
+
+card1.grid(row=0, column=0, padx=20)
+card1.pack_propagate(False)
+
+tk.Label(
+    card1,
+    text="TOTAL WINES",
+    bg=CARD,
+    fg=GOLD,
+    font=("Segoe UI", 12, "bold")
+).pack(pady=10)
+
+tk.Label(
+    card1,
+    text="0",
+    bg=CARD,
+    fg=WHITE,
+    font=("Segoe UI", 30, "bold")
+).pack()
+
+
+card2 = tk.Frame(
+    stats_frame,
+    bg=CARD,
+    width=250,
+    height=130,
+    highlightbackground=WINE_RED,
+    highlightthickness=2
+)
+
+card2.grid(row=0, column=1, padx=20)
+card2.pack_propagate(False)
+
+tk.Label(
+    card2,
+    text="SALES TODAY",
+    bg=CARD,
+    fg=WINE_RED,
+    font=("Segoe UI", 12, "bold")
+).pack(pady=10)
+
+tk.Label(
+    card2,
+    text="0",
+    bg=CARD,
+    fg=WHITE,
+    font=("Segoe UI", 30, "bold")
+).pack()
